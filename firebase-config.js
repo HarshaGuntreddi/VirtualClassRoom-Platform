32e47f3c2b42937f8093715769ed9059aaf34ed3
HarshaGuntreddi <beharshaguntreddi@gmail.com>
2025-04-03 01:44:27 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAP3W9dAu1l_oFioG-R1vLAuuwk2bOlmaU",
+    authDomain: "attendance-d4006.firebaseapp.com",
+    projectId: "attendance-d4006",
+    storageBucket: "attendance-d4006.firebasestorage.app",
+    messagingSenderId: "434247413379",
+    appId: "1:434247413379:web:2d068bfa024b019ef435b6",
+    measurementId: "G-MD6ZJWZ80P"
+};
